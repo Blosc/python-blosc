e76afe5b198311b86e02d1254f8433da209284b5
Francesc Alted <francesc@continuum.io>
2012-09-15 18:32:41 +0200

    Improved version of Blosc, without exit() in blosc_compress() and a redefinition of BLOSC_MAX_BUFFERSIZE

--- c-blosc/blosc.h
+++ c-blosc/blosc.h
@@ -14,11 +14,11 @@
 /* Version numbers */
 #define BLOSC_VERSION_MAJOR    1    /* for major interface/format changes  */
 #define BLOSC_VERSION_MINOR    1    /* for minor interface/format changes  */
-#define BLOSC_VERSION_RELEASE  3    /* for tweaks, bug-fixes, or development */
+#define BLOSC_VERSION_RELEASE  4    /* for tweaks, bug-fixes, or development */
 
-#define BLOSC_VERSION_STRING   "1.1.3"  /* string version.  Sync with above! */
-#define BLOSC_VERSION_REVISION "$Rev: 296 $"   /* revision version */
-#define BLOSC_VERSION_DATE     "$Date:: 2010-11-16 #$"    /* date version */
+#define BLOSC_VERSION_STRING   "1.1.4.dev"  /* string version.  Sync with above! */
+#define BLOSC_VERSION_REVISION "$Rev: 297 $"   /* revision version */
+#define BLOSC_VERSION_DATE     "$Date:: 2012-09-15 #$"    /* date version */
 
 /* The *_VERS_FORMAT should be just 1-byte long */
 #define BLOSC_VERSION_FORMAT    2   /* Blosc format version, starting at 1 */
@@ -36,7 +36,7 @@
 #define BLOSC_MAX_OVERHEAD BLOSC_MIN_HEADER_LENGTH
 
 /* Maximum buffer size to be compressed */
-#define BLOSC_MAX_BUFFERSIZE INT_MAX   /* Signed 32-bit internal counters */
+#define BLOSC_MAX_BUFFERSIZE (INT_MAX - BLOSC_MAX_OVERHEAD)  /* Signed 32-bit internal counters */
 
 /* Maximum typesize before considering buffer as a stream of bytes */
 #define BLOSC_MAX_TYPESIZE 255         /* Cannot be larger than 255 */
