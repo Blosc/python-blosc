8c90e0dc2f4d4f8054284584a901f819c653ba04
Valentin Haenel <valentin.haenel@gmx.de>
2013-04-12 17:25:36 +0200

    testing exceptions raises during compression_ptr

--- blosc/test.py
+++ blosc/test.py
@@ -1,4 +1,5 @@
 import unittest
+import ctypes
 import blosc
 
 class TestCodec(unittest.TestCase):
@@ -21,6 +22,32 @@ class TestCodec(unittest.TestCase):
         self.assertRaises(ValueError, blosc.compress,
                 'a' * (blosc.BLOSC_MAX_BUFFERSIZE+1), typesize=1)
 
+    def test_compress_ptr_exceptions(self):
+        # Make sure we do have a valid address, to reduce the chance of a
+        # segfault if we do actually start compressing because the exceptions
+        # aren't raised.
+
+        import ctypes
+        typesize = 8
+        items = 8
+        data = [float(i) for i in range(items)]
+        Array = ctypes.c_double * items
+        array = Array(*data)
+        address = ctypes.addressof(array)
+
+        self.assertRaises(ValueError, blosc.compress_ptr, address, items,
+                typesize=typesize, clevel=-1)
+        self.assertRaises(ValueError, blosc.compress_ptr, address, items,
+                typesize=typesize, clevel=10)
+
+
+        self.assertRaises(TypeError, blosc.compress_ptr, 1.0, items,
+                typesize=typesize)
+        self.assertRaises(TypeError, blosc.compress_ptr, ['abc'], items,
+                typesize=typesize)
+
+        self.assertRaises(ValueError, blosc.compress_ptr, address,
+                blosc.BLOSC_MAX_BUFFERSIZE+1, typesize=typesize)
 
 if __name__ == '__main__':
         unittest.main()
