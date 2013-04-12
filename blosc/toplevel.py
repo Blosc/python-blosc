ced6aaa18b2c7b44f411300f01bec903021bc271
Valentin Haenel <valentin.haenel@gmx.de>
2013-04-12 17:32:26 +0200

    remove spurious line in docstring

--- blosc/toplevel.py
+++ blosc/toplevel.py
@@ -207,7 +207,6 @@ def compress_ptr(address, items, typesize, clevel=9, shuffle=True):
     True
 
     >>> import ctypes
-    >>> Array23 = ctypes.c_double
     >>> typesize = 8
     >>> data = [float(i) for i in range(items)]
     >>> Array = ctypes.c_double * items
@@ -310,7 +309,6 @@ def decompress_ptr(bytesobj, address):
     True
 
     >>> import ctypes
-    >>> Array23 = ctypes.c_double
     >>> typesize = 8
     >>> data = [float(i) for i in range(items)]
     >>> Array = ctypes.c_double * items
