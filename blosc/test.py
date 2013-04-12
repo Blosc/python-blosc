487ceb65a0729339e21ccf944fa2845cc53ba04c
Valentin Haenel <valentin.haenel@gmx.de>
2013-04-12 17:04:08 +0200

    testing exceptions raises during compression

--- blosc/test.py
+++ blosc/test.py
@@ -9,5 +9,18 @@ class TestCodec(unittest.TestCase):
         d = blosc.decompress(c)
         self.assertEqual(s, d)
 
+    def test_compress_exceptions(self):
+        s = '0123456789'
+
+        self.assertRaises(ValueError, blosc.compress, s, typesize=1, clevel=-1)
+        self.assertRaises(ValueError, blosc.compress, s, typesize=1, clevel=10)
+
+        self.assertRaises(ValueError, blosc.compress, 1.0, 1)
+        self.assertRaises(ValueError, blosc.compress, ['abc'], 1)
+
+        self.assertRaises(ValueError, blosc.compress,
+                'a' * (blosc.BLOSC_MAX_BUFFERSIZE+1), typesize=1)
+
+
 if __name__ == '__main__':
         unittest.main()
