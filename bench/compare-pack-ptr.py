e7e1ffacbf0111218fd74b42e8e4e979ea1820bd
Francesc Alted <francesc@continuum.io>
2013-05-04 14:37:25 +0200

    Bench script adapted to Python 3 and reduced memory needs (so as it can run on 32-bit Win)

--- bench/compare-pack-ptr.py
+++ bench/compare-pack-ptr.py
@@ -21,24 +21,24 @@ It is up to the user to decide among the convenience of `pack_array` /
 `uncompress_ptr`.
 
 """
-
+from __future__ import print_function
 import numpy
 import time
 import blosc
 
-N = 3e7
+N = 1e7
 
-print "Creating a large NumPy array with %d int64 elements..." % N
+print("Creating a large NumPy array with %d int64 elements..." % N)
 in_ = numpy.arange(N, dtype=numpy.int64)
-print in_
+print(in_)
 
 tic = time.time()
 c = blosc.pack_array(in_)
 out = blosc.unpack_array(c)
 assert((in_ == out).all())
 toc = time.time()
-print "Time for pack_array/unpack_array:     %.3f s." % (toc-tic,),
-print "\tCompr ratio: %.2f" % (in_.size*in_.dtype.itemsize*1. / len(c))
+print("Time for pack_array/unpack_array:     %.3f s." % (toc-tic,), end='')
+print("\tCompr ratio: %.2f" % (in_.size*in_.dtype.itemsize*1. / len(c)))
 
 tic = time.time()
 c = blosc.compress_ptr(in_.__array_interface__['data'][0],
@@ -47,6 +47,6 @@ out = numpy.empty(in_.size, dtype=in_.dtype)
 blosc.decompress_ptr(c, out.__array_interface__['data'][0])
 assert((in_ == out).all())
 toc = time.time()
-print "Time for compress_ptr/decompress_ptr: %.3f s." % (toc-tic,),
-print "\tCompr ratio: %.2f" % (in_.size*in_.dtype.itemsize*1. / len(c))
+print("Time for compress_ptr/decompress_ptr: %.3f s." % (toc-tic,), end='')
+print("\tCompr ratio: %.2f" % (in_.size*in_.dtype.itemsize*1. / len(c)))
 
