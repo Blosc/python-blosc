518757e52239bcd85c25c650cfdc3d5bfe96dce4
Valentin Haenel <valentin@haenel.co>
2018-11-20 11:51:53 +0100

    remove setup.py checks for python 2.6

--- setup.py
+++ setup.py
@@ -28,13 +28,13 @@ def exit_with_error(message):
 
 # Check for Python
 if sys.version_info[0] == 2:
-    if sys.version_info[1] < 6:
-        exit_with_error("You need Python 2.6 or greater to install blosc!")
+    if sys.version_info[1] < 7:
+        exit_with_error("You need Python 2.7 or greater to install blosc!")
 elif sys.version_info[0] == 3:
     if sys.version_info[1] < 3:
         exit_with_error("You need Python 3.3 or greater to install blosc!")
 else:
-    exit_with_error("You need Python 2.6/3.3 or greater to install blosc!")
+    exit_with_error("You need Python 2.7/3.3 or greater to install blosc!")
 
 tests_require = ['numpy']
 if sys.version_info[:2] < (2, 7):
