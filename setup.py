84d59d9a5c2feac09c96184e6b740793ff22f1ad
Valentin Haenel <valentin@haenel.co>
2019-02-03 11:11:00 +0100

    fix license listing in setup.py

--- setup.py
+++ setup.py
@@ -179,7 +179,7 @@ if __name__ == '__main__':
     Intended Audience :: Developers
     Intended Audience :: Information Technology
     Intended Audience :: Science/Research
-    License :: OSI Approved :: MIT License
+    License :: OSI Approved :: BSD License
     Programming Language :: Python
     Programming Language :: Python :: 2.7
     Programming Language :: Python :: 3.4
