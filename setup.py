e3f80ed6a547a1dd680062ef5ae71586026b065f
Lehman Garrison <lgarrison@flatironinstitute.org>
2020-04-10 14:08:44 -0400

    Add AVX2 cpuinfo check to setup.py

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
         cmake_args = [
           '-DBLOSC_DIR:PATH=%s' % os.environ.get('BLOSC_DIR', ''),
           '-DDEACTIVATE_SSE2:BOOL=%s' % cmake_bool(('DISABLE_BLOSC_SSE2' in os.environ) or (cpu_info is None) or ('sse2' not in cpu_info['flags'])),
-          '-DDEACTIVATE_AVX2:BOOL=%s' % cmake_bool('DISABLE_BLOSC_AVX2' in os.environ),
+          '-DDEACTIVATE_AVX2:BOOL=%s' % cmake_bool(('DISABLE_BLOSC_AVX2' in os.environ) or (cpu_info is None) or ('avx2' not in cpu_info['flags'])),
           '-DDEACTIVATE_LZ4:BOOL=%s' % cmake_bool(not int(os.environ.get('INCLUDE_LZ4', '1'))),
           # Snappy is disabled by default
           '-DDEACTIVATE_SNAPPY:BOOL=%s' % cmake_bool(not int(os.environ.get('INCLUDE_SNAPPY', '0'))),
